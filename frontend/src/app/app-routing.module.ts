75dbcb9616d44bcfff2da68e7855c54e98da7f64
Preedhi Garg <preedhigarg@Preedhis-MacBook-Air.local>
2023-05-18 14:34:53 -0700

    Route cleanup

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-// import { HomeComponent } from './components/home/home/home.component';
-import { NavComponent } from './components/partials/navbar/nav.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
 import { ReadComponent } from './components/crudpage/read/read.component';
 import { CreateComponent } from './components/crudpage/create/create.component';
-import { MessagesComponent } from './components/crudpage/messages/messages.component';
 import { CategoryComponent } from './components/category/category.component';
 import { TimelineComponent } from './components/timeline/timeline.component';
 import { SummaryComponent } from './components/summary/summary.component';
@@ -15,7 +12,6 @@ import { UpdateComponent } from './components/crudpage/update/update.component';
 const routes: Routes = [
   // adding the route to home
   { path: 'welcome', component: WelcomeComponent },
-  //{ path: 'partials/nav', component: NavComponent }, // This is used for debugging we dont need this as a standalone route
   { path: 'read', component: ReadComponent },
   { path: 'create', component: CreateComponent },
   { path: 'category', component:CategoryComponent},
