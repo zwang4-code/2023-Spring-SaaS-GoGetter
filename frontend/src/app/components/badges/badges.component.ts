9b55de2be5134bf4626575d4cede9926d32512bd
zwang4-code <zwang10312@gmail.com>
2023-05-30 12:22:22 -0700

    implemented count goal logic for badge selection

--- frontend/src/app/components/badges/badges.component.ts
+++ frontend/src/app/components/badges/badges.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { GoalService } from 'src/app/service/goal-service.service';
+import { IGoalModelAngular } from 'src/app/share/model/IGoalModelAngular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-badges',
@@ -6,5 +11,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./badges.component.css']
 })
 export class BadgesComponent {
+  goalsObservable: Observable<IGoalModelAngular[]>;
+  numOfGoals = 0;
 
+  constructor(private goalService: GoalService, private router: Router, activatedRoute: ActivatedRoute) {
+    // This is for getting all of the goals
+    this.goalsObservable = goalService.getAllGoals();
+    this.goalsObservable.subscribe((result) => {
+      this.numOfGoals = result.length
+      console.log("user's total number of goals: ", this.numOfGoals)
+    })
+  }
 }
