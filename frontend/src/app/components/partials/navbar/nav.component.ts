109d90c491bc9f6a821fbda5158afd5add8e588c
Preedhi Garg <preedhigarg@Preedhis-MacBook-Air.local>
2023-05-30 22:57:41 -0700

    user info UI'

--- frontend/src/app/components/partials/navbar/nav.component.ts
+++ frontend/src/app/components/partials/navbar/nav.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GoalService } from '../../../service/goal-service.service';
+import { IUserModelAngular } from 'src/app/share/model/IUserModelAngular';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
-export class NavComponent {
+export class NavComponent implements OnInit {
+  userObservable!: Observable<IUserModelAngular>;
+  user: IUserModelAngular;
+
+  constructor(private goalService: GoalService) {
+    this.user = {} as IUserModelAngular;
+  }
+
+  ngOnInit() {
+    this.userObservable = this.goalService.getUserById();
+    this.userObservable.subscribe((result) => {
+      this.user = result;
+    })
+  }
 
 }
