bd5f1f5025627acc54d7fb95e3a47ef7b65dd8ce
Preedhi Garg <preedhigarg@Preedhis-MacBook-Air.local>
2023-05-31 18:04:31 -0700

    badge

--- frontend/src/app/components/badges/badges.component.ts
+++ frontend/src/app/components/badges/badges.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 import { GoalService } from 'src/app/service/goal-service.service';
-import { IGoalModelAngular } from 'src/app/share/model/IGoalModelAngular';
+import { IUserModelAngular } from 'src/app/share/model/IUserModelAngular';
 import { Router } from '@angular/router';
 
 @Component({
@@ -11,15 +11,15 @@ import { Router } from '@angular/router';
   styleUrls: ['./badges.component.css']
 })
 export class BadgesComponent {
-  goalsObservable: Observable<IGoalModelAngular[]>;
-  numOfGoals = 0;
+  userObservable!: Observable<IUserModelAngular>;
+  user: IUserModelAngular;
 
   constructor(private goalService: GoalService, private router: Router, activatedRoute: ActivatedRoute) {
     // This is for getting all of the goals
-    this.goalsObservable = goalService.getAllGoals();
-    this.goalsObservable.subscribe((result) => {
-      this.numOfGoals = result.length
-      console.log("user's total number of goals: ", this.numOfGoals)
+    this.user = {} as IUserModelAngular;
+    this.userObservable = this.goalService.getUserById();
+    this.userObservable.subscribe((result) => {
+      this.user = result;
     })
   }
 }
