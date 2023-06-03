e598838407f58175c1a6cbbbd713424cac2dee29
sharonleo-su <sleo@seattleu.edu>
2023-06-02 20:33:05 -0700

    Display pie charts on summary page to group goals by category and progess state.

--- frontend/src/app/components/summary/summary.component.ts
+++ frontend/src/app/components/summary/summary.component.ts
@@ -1,46 +1,125 @@
-import { Component, OnInit } from '@angular/core';
-import Chart from 'chart.js/auto';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import Chart, { ChartConfiguration, ChartTypeRegistry } from 'chart.js/auto';
+import { GoalService } from 'src/app/service/goal-service.service';
+import { IGoalModelAngular } from 'src/app/share/model/IGoalModelAngular';
+import { ProgressEnum } from 'src/app/share/enum/ProgressEnum';
+import { CategoryEnum } from 'src/app/share/enum/CategoryEnum';
+
 
 @Component({
   selector: 'app-summary',
   templateUrl: './summary.component.html',
   styleUrls: ['./summary.component.css']
 })
-export class SummaryComponent implements OnInit{
-  public chart: any;
-  ngOnInit() {
-    const chartData = [
-      {
-        label: 'Health',
-        value: 20,
-        backgroundColor: 'rgb(204, 204, 255)'
-      },
-      {
-        label: 'Career',
-        value: 30,
-        backgroundColor: 'rgb(128, 0, 128)'
-      },
-      {
-        label: 'School',
-        value: 50,
-        backgroundColor: 'rgb(173, 216, 224 )'
-      }
-    ];
+export class SummaryComponent implements OnInit {
+  @ViewChild('categoryChartCanvas') categoryChartCanvas!: ElementRef;
+  @ViewChild('progressChartCanvas') progressChartCanvas!: ElementRef;
 
-    this.chart = new Chart("MyChart", {
+  private categoryColors: string[] = [
+    '#ff6384',   // Category 1
+    '#36a2eb',   // Category 2
+    '#ffce56',   // Category 3
+    '#4bc0c0',   // Category 4
+    '#9966cc',   // Category 5
+    '#ff8c00'    // Category 6
+  ];
+
+  constructor(private goalService: GoalService) { }
+
+  ngOnInit() {
+    this.goalService.getAllGoals().subscribe((goals: IGoalModelAngular[]) => {
+      this.createCategoryChart(goals);
+      this.createProgressChart(goals);
+    });
+  }
+
+  createCategoryChart(goals: IGoalModelAngular[]) {
+    const chartData = this.getChartDataByCategory(goals);
+    const chartOptions = {
+      responsive: true,
+      aspectRatio: 3.5,
+      plugins: {
+        legend: {
+          display: true,
+          labels: {
+            filter: (legendItem: { index: number; }, chartData: any) => legendItem.index !== 0  // Filter out the first label
+          } 
+        }
+      }
+    };
+    this.createPieChart(this.categoryChartCanvas.nativeElement, chartData.labels, chartData.data, this.categoryColors, chartOptions);
+  }
+
+  createProgressChart(goals: IGoalModelAngular[]) {
+    const chartData = this.getChartDataByProgress(goals);
+    const progressColors = [
+      '#ff6384',   // Not Started
+      '#36a2eb',   // In Progress
+      '#4bc0c0'   // Completed
+    ];
+    const chartOptions = {
+      responsive: true,
+      aspectRatio: 3.5,
+      plugins: {
+        legend: {
+          display: true, 
+        }
+      }
+    };
+
+    this.createPieChart(this.progressChartCanvas.nativeElement, chartData.labels, chartData.data, progressColors, chartOptions);
+  }
+
+  createPieChart(canvas: HTMLCanvasElement, labels: string[], data: number[], backgroundColor: string[], options: any): Chart<keyof ChartTypeRegistry, (number | null)[]> {
+    const chartConfig: ChartConfiguration<'pie', number[], string> = {
       type: 'pie',
       data: {
-        labels: chartData.map(data => data.label),
+        labels: labels,
         datasets: [{
-          data: chartData.map(data => data.value),
-          backgroundColor: chartData.map(data => data.backgroundColor)
+          data: data,
+          backgroundColor: backgroundColor
         }]
       },
-      options: {
-        responsive: true,
-        aspectRatio:3.5
-      }
+      options: options
+    };
+
+    return new Chart(canvas, chartConfig as any);
+  }
+
+
+  getChartDataByCategory(goals: IGoalModelAngular[]): { labels: string[], data: number[], backgroundColor: string[] } {
+    const categories: CategoryEnum[] = Object.values(CategoryEnum);
+    const labels: string[] = [];
+    const data: number[] = [];
+    const backgroundColor: string[] = [];
+
+    categories.forEach((category: CategoryEnum) => {
+      const goalsInCategory = goals.filter(goal => goal.category === category);
+      const count = goalsInCategory.length;
+
+      labels.push(category);
+      data.push(count);
+      backgroundColor.push(...this.categoryColors.slice(0, labels.length));
     });
+
+    return { labels, data, backgroundColor };
+  }
+
+  getChartDataByProgress(goals: IGoalModelAngular[]): { labels: string[], data: number[], backgroundColor: string[] } {
+    const progressValues: string[] = Object.values(ProgressEnum);
+    const labels: string[] = [];
+    const data: number[] = [];
+    const backgroundColor: string[] = [];
+
+    progressValues.forEach((progress: string) => {
+      const goalsInProgress = goals.filter(goal => goal.progress === progress);
+      const count = goalsInProgress.length;
+
+      labels.push(progress);
+      data.push(count);
+    });
+
+    return { labels, data, backgroundColor };
   }
 
 }
