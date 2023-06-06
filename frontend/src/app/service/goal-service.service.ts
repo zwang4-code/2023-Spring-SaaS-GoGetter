a0c161c1e471787dc5a5fef77a7cd14d66f445ae
Preedhi Garg <preedhigarg@Preedhis-MacBook-Air.local>
2023-06-05 22:30:53 -0700

    http change

--- frontend/src/app/service/goal-service.service.ts
+++ frontend/src/app/service/goal-service.service.ts
@@ -14,8 +14,8 @@ export class GoalService {
   // appGoalURL = 'http://localhost:8080/app/goal'; 
   // userURL = 'http://localhost:8080/app/user';
   
-  appGoalURL = 'https://gogetterbebetter.azurewebsites.net/app/goal'; 
-  userURL = 'https://gogetterbebetter.azurewebsites.net/app/user';
+  appGoalURL = 'http://gogetterbebetter.azurewebsites.net/app/goal'; 
+  userURL = 'http://gogetterbebetter.azurewebsites.net/app/user';
 
   constructor(private http: HttpClient) {
   }
